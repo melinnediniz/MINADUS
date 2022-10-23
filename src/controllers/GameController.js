45bf8d18051ff3a2107e3c973b108abbbf653ce0
FMABr <felipemuamorim@gmail.com>
2022-10-23 16:54:14 -0400

    Generate daily game

--- src/controllers/GameController.js
+++ src/controllers/GameController.js
@@ -1,5 +1,56 @@
+import random from "seedrandom";
+
+const secret = process.env.SEED_SECRET;
+
+function mineField(rng, height, width, bombCount) {
+    const field = new Array(height);
+    for (let row = 0; row < height; row++) {
+        field[row] = new Array(width);
+
+        for (let column = 0; column < width; column++) {
+            field[row][column] = 0;
+        }
+    }
+
+    const cells = height * width;
+    let bombs =
+        bombCount < height * width ? bombCount : Math.floor(cells * 0.33);
+
+    for (; bombs > 0; bombs--) {
+        const row = Math.floor(rng() * height);
+        const column = Math.floor(rng() * width);
+
+        addMine(field, row, column);
+    }
+
+    return field;
+}
+
+function getDailySeed() {
+    const date = new Date().toISOString().substring(0, 10);
+    return date + secret;
+}
+
+function addMine(field, row, column) {
+    if (field[row] && Number.isInteger(field[row][column])) {
+        field[row][column] = 9;
+
+        for (let i = -1; i <= 1; i++)
+            for (let j = -1; j <= 1; j++)
+                if (
+                    Number.isInteger(
+                        field[row + i][column + j] &&
+                            field[row + i][column + j] < 8
+                    )
+                )
+                    ++field[row + i][column + j];
+    }
+}
+
 function getDailyGame() {
-  // MinesWeeper generation here...
+    const dailyRNG = random(getDailySeed());
+
+    return mineField(dailyRNG, 10, 10);
 }
 
 export { getDailyGame };
