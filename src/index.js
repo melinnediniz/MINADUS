624d341cdb6d05b52fe43c3e711da8ec96c807b7
FMABr <felipemuamorim@gmail.com>
2022-10-26 09:10:52 -0400

    CORS + Mongoose database promise

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import * as dotenv from "dotenv";
 dotenv.config();
 import express from "express";
+import cors from 'cors';
 import { connectWithDB } from "./connections.js";
 import { Routes } from "./routes/index.js";
 
@@ -8,10 +9,14 @@ const app = express();
 const port = process.env.PORT || 3333;
 
 const dbURL = process.env.DB_URL || "http://localhost:1342";
-await connectWithDB(dbURL)
-    .then(() => console.log("Database connected..."))
-    .catch(() => console.log("Error on connect with database."));
+try {
+    await connectWithDB(dbURL);
+    console.log("Database connected...");
+} catch {
+    console.log("Error on connect with database.");
+}
 
+app.use(cors());
 app.use(express.json());
 app.use(Routes);
 app.listen(port, () => {
